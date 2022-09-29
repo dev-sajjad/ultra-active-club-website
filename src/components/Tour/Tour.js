fe9d6f36bb65b22fdf34e08b9c409b131d373902
dev-sajjad <sajjadbmarpc@gmail.com>
2022-09-29 18:42:52 +0600

    created initial page

---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Tour.css'
+
+const Tour = () => {
+    return (
+        <div className='tour-container'>
+            <div>
+                <h2>tour plan</h2>
+            </div>
+            <div>
+                <h2>tour details</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Tour;
